7eb1d4a9b0fe7e4e037b5d8f06b471f59b27e6f7
chettipally likhitha <chettipallylikhitha@gmail.com>
2026-05-29 16:55:04 +0530

    add process route

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,22 @@ import express from "express";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "API working"
-  });
+app.post("/api/process", async (req, res) => {
+  try {
+    res.json({
+      success: true,
+      message: "Process API working"
+    });
+  } catch (error: any) {
+    console.error(error);
+
+    res.status(500).json({
+      error: error.message
+    });
+  }
 });
 
 export default app;
