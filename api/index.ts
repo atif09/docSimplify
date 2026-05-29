893485c8a1b90354802359d877fcb0092618223e
chettipally likhitha <chettipallylikhitha@gmail.com>
2026-05-29 17:56:36 +0530

    Refactor serverless function entrypoint for Vercel API compatibility

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,3 @@
-import express from "express";
+import app from "../server";
 
-const app = express();
-
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ extended: true, limit: "25mb" }));
-
-app.post("/api/process", async (req, res) => {
-  try {
-    res.json({
-      success: true,
-      message: "Process API working"
-    });
-  } catch (error: any) {
-    console.error(error);
-
-    res.status(500).json({
-      error: error.message
-    });
-  }
-});
-
-export default app;
+export default app;
